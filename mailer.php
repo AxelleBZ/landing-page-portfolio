f2601650f262c5595680143b883708ccae53e42c
AxelleBZ <axellezeller@hotmail.fr>
2020-01-18 21:49:10 -0500

    update mailer

--- mailer.php
+++ mailer.php
@@ -55,13 +55,13 @@
   $apiKey = getenv('SENDGRID_API_KEY');
   $sg = new \SendGrid($apiKey);
 
-  // $response = $sg->client->mail()->send()->post($request_body);
+  $response = $sg->client->mail()->send()->post($request_body);
   // echo $response->statusCode();
   // echo $response->body();
   // echo $response->headers();
 
   // Send the email.
-  if ($sg->client->mail()->send()->post($request_body)) {
+  if ($response->statusCode() == 200) {
       // Set a 200 (okay) response code.
       http_response_code(200);
       echo "Thank You! Your message has been sent.";
